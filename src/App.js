9753c5e1c99f7a244fffad17d01b8f1fdb032f1f
Naufal Arie <naufalarie98@gmail.com>
2022-03-22 22:44:45 +0700

    First Homework 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <img className="image" src='https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b' alt="bohemian"></img>
+        <p>Bohemian Rhapsody (The Original Soundtrack)</p>
+        <p>Queen</p>
+        <button type='button' onClick="alert('select')">select</button>
+      </div>
     </div>
   );
 }
