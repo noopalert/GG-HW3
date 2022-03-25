3e80765d08a9dc3c46455f80713d057b8ac57487
Naufal Arie <naufalarie98@gmail.com>
2022-03-25 14:33:20 +0700

    first homework 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import data from './data/data.js'
-
+import './components/style.css';
+import Gif from '../src/components/index';
 
 function App() {
   return (
-    <div className="App">
-      <div className='container'>
-        <img className="image" src={data.album.images[1].url} alt="bohemian"></img>
-        <p>{data.album.name}</p>
-        <p>{data.album.artists[0].name}</p>
-        <button type='button' onClick="alert('select')">Select</button>
-      </div>
-    </div>
+    <Gif />
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import data from '../data/data';
+function Gif (){
+    return(
+        <div className="App">
+        <div className='container'>
+            <img className="image" src={data.album.images[1].url} alt="bohemian"></img>
+            <p>{data.album.name}</p>
+            <p>{data.album.artists[0].name}</p>
+            <button type='button' onClick="alert('select')">Select</button>
+        </div>
+    </div>
+    );
+}
+export default Gif;
